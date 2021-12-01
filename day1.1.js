79e0b3c8e527779588aae7dc81c060434d9a2d98
Pau <kapilpau@147ddab8fdb0.ant.amazon.com>
2021-12-01 16:24:47 +0000

    Day 1 Challenge 1

---
+++ day1.1.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+let input;
+
+try {
+    input = fs.readFileSync("./input1.txt", "utf8").split("\n");
+} catch (e) {
+    console.error("Error reading in file")
+    console.error(e);
+    process.exit(1);
+}
+
+let prev = parseInt(input[0])
+let count = 0;
+
+for (let i = 1; i<input.length; i++) {
+    const curr = parseInt(input[i])
+    if (curr > prev)
+        count++;
+    prev = curr
+}
+
+console.log("Answer:", count)
