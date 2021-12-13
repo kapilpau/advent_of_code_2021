e76bd7693fc8c8ed9fbee76301113b303edd9563
Kapil Pau <kapilpau@amazon.com>
2021-12-13 18:18:24 +0000

    Day 11 Challenge 1

---
+++ day11.1.js
@@ -0,0 +1,97 @@
+const fs = require('fs');
+
+let input;
+
+try {
+    input = fs.readFileSync("./inputs/input11.txt", "utf8").split("\n");
+} catch (e) {
+    console.error("Error reading in file")
+    console.error(e);
+    process.exit(1);
+}
+
+const steps = 100;
+let count = 0;
+
+let octopuses = input.map(i => i.split("").map(x => parseInt(x)));
+const h = octopuses.length - 1;
+const w = octopuses[0].length - 1;
+
+for (let step = 0; step<steps; step++) {
+    let flashes = [];
+
+    for (let row = 0; row < octopuses.length; row++) {
+        for (let col = 0; col < octopuses[row].length; col++) {
+            if (octopuses[row][col] === 9) {
+                count++;
+                octopuses[row][col] = 0
+                flashes.push([row, col])
+            } else {
+                octopuses[row][col]++;
+            }
+        }
+    }
+
+    flashes.forEach(flash => {
+        count += ripple(...flash, octopuses, h, w)
+    })
+}
+
+console.log("Answer", count)
+
+function ripple(y, x, grid, h, w) {
+    let counter = 0;
+    const left = x - 1;
+    const right = x + 1;
+    const up = y - 1;
+    const down = y + 1;
+
+    if (left !== -1 && grid[y][left] !== 0 && ++grid[y][left] === 10) {
+            grid[y][left] = 0;
+            counter++;
+            counter += ripple(y, left, grid, h, w)
+    }
+
+    if (up !== -1 && grid[up][x] !== 0 && ++grid[up][x] === 10) {
+        grid[up][x] = 0;
+        counter++;
+        counter += ripple(up, x, grid, h, w)
+    }
+
+    if (right <= w && grid[y][right] !== 0 && ++grid[y][right] === 10) {
+        grid[y][right] = 0;
+        counter++;
+        counter += ripple(y, right, grid, h, w)
+    }
+
+    if (down <= h && grid[down][x] !== 0 && ++grid[down][x] === 10) {
+        grid[down][x] = 0;
+        counter++;
+        counter += ripple(down, x, grid, h, w)
+    }
+
+    if ((down <= h) && (right <= w) && grid[down][right] !== 0 && ++grid[down][right] === 10) {
+        grid[down][right] = 0;
+        counter++;
+        counter += ripple(down, right, grid, h, w)
+    }
+
+    if ((up !== -1) && (left !== -1) && grid[up][left] !== 0 && ++grid[up][left] === 10) {
+        grid[up][left] = 0;
+        counter++;
+        counter += ripple(up, left, grid, h, w)
+    }
+
+    if ((up !== -1) && (right <= w) && grid[up][right] !== 0 && ++grid[up][right] === 10) {
+        grid[up][right] = 0;
+        counter++;
+        counter += ripple(up, right, grid, h, w)
+    }
+
+    if ((down <= h) && (left !== -1) && grid[down][left] !== 0 && ++grid[down][left] === 10) {
+        grid[down][left] = 0;
+        counter++;
+        counter += ripple(down, left, grid, h, w)
+    }
+    return counter;
+}
